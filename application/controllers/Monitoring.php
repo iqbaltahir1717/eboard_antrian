34261416e2ecb0a143ae288a3b76c2a74d967352
iqbaltahir33 <iqbalknightphantom33@gmail.com>
2023-01-05 16:05:24 +0800

    add change

--- application/controllers/Monitoring.php
+++ application/controllers/Monitoring.php
@@ -18,7 +18,7 @@ class Monitoring extends CI_Controller
 		$data['spesialis']   = $this->m_spesialis->read('', '', '');
 		$data['antrian']   = $this->m_antrian->read('', '', '');
 		$data['dokter']   = $this->m_user->read_group('', '', '', 4);
-		
+
 
 		// echo "<pre>";
 		// print_r($data['spesialis']);
@@ -29,5 +29,18 @@ class Monitoring extends CI_Controller
 		$view         = "_frontend/monitoring";
 		$viewCategory = "all";
 		renderTemplateFront($data, $view, $viewCategory);
+	}
+
+	public function realtime()
+	{
+		header("Access-Control-Allow-Origin: *");
+		header("Content-Type: application/json; charset=UTF-8");
+		http_response_code(200);
+
+
+		$data    = $this->m_spesialis->read('', '', '');
+		$resultData = $data;
+
+		echo json_encode($resultData, JSON_PRETTY_PRINT);
 	}
 }
--- application/views/_frontend/monitoring.php
+++ application/views/_frontend/monitoring.php
@@ -1,7 +1,7 @@
 <section id="monitoring" class="d-flex justify-content-center align-content-center" style="flex-wrap: wrap;">
     <?php foreach ($spesialis as $s) {  ?>
         <div class="col-lg-2 box-item">
-            <h3><?php echo $s->spesialis_kode_antrian . $s->antrian_saat_ini; ?></h3>
+            <h3 id="antrian<?= $s->spesialis_id ?>"><?php echo $s->spesialis_kode_antrian . $s->antrian_saat_ini; ?></h3>
             <h4>Antrian Saat Ini</h4>
         </div>
     <?php } ?>
@@ -14,3 +14,28 @@
         </div>
     <?php } ?>
 </section>
+
+<script>
+    $(document).ready(function() {
+        function antrian() {
+            url = "<?php echo base_url(); ?>monitoring/realtime";
+            $.get(url, function(data, status) {
+                data = JSON.parse(data);
+
+                <?php $i = 0;
+                foreach ($spesialis as $s) { ?>
+                    antrian_nomor<?= $s->spesialis_id ?> = data[<?= $i ?>]['antrian_saat_ini'];
+                    $("#antrian<?= $s->spesialis_id ?>").html(antrian_nomor<?= $s->spesialis_id ?>);
+                <?php $i++;
+                } ?>
+
+            });
+        }
+        antrian();
+
+        setInterval(function() {
+            antrian();
+        }, 3000)
+
+    });
+</script>
